1e2b1a43ba450edbca7723439a72145523a5562c
lakmalishu95 <isurulakmal858@gmail.com>
2023-06-23 13:40:41 +0530

    added ReportPage.js and added app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route,Switch, Redirect } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
+import ReportPage from './components/ReportPage';
 
 function App() {
-  return (
-    <div className="App">
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userData, setUserData] = useState(null);
 
- 
-        <LoginPage />
-    </div>
+  const handleLogin = (userData) => {
+    setIsLoggedIn(true);
+    setUserData(userData);
+  };
+
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/login">
+            {isLoggedIn ? <Redirect to="/report" /> : <LoginPage handleLogin={handleLogin} />}
+          </Route>
+          <Route path="/report">
+            {!isLoggedIn ? <Redirect to="/login" /> : <ReportPage />}
+          </Route>
+          <Redirect to="/login" />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/ReportPage.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Box, Typography } from '@mui/material';
+
+const DETAILED_TRANSACTION_API = 'http://148.251.225.118:3200/api/detailed-transaction';
+const TRANSACTION_SUMMARY_API = 'http://148.251.225.118:3200/api/transaction-summary';
+
+function ReportPage() {
+  const [transactionData, setTransactionData] = useState([]);
+  const [transactionSummary, setTransactionSummary] = useState(null);
+
+  useEffect(() => {
+    fetchTransactionData();
+    fetchTransactionSummary();
+  }, []);
+
+  const fetchTransactionData = async () => {
+    try {
+      const response = await axios.get(DETAILED_TRANSACTION_API);
+      const transactionList = response.data.transaction.transaction_list;
+      setTransactionData(transactionList);
+    } catch (error) {
+      console.error('Error fetching transaction data:', error);
+    }
+  };
+
+  const fetchTransactionSummary = async () => {
+    try {
+      const response = await axios.get(TRANSACTION_SUMMARY_API);
+      const summaryData = response.data.summery;
+      setTransactionSummary(summaryData);
+    } catch (error) {
+      console.error('Error fetching transaction summary:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Report Page</h2>
+      <Box>
+        {transactionSummary && (
+          <div>
+            <Typography variant="h6">Transaction Summary</Typography>
+            <Typography>Total Transaction Count: {transactionSummary.totalTransactionCount}</Typography>
+            <Typography>Total Revenue: {transactionSummary.totalRevenue}</Typography>
+            <Typography>Total Card Transaction: {transactionSummary.totalCardTransaction}</Typography>
+            <Typography>Total Cash Transaction: {transactionSummary.totalCashTransaction}</Typography>
+            <Typography>Total QR Transaction: {transactionSummary.totalQrTransaction}</Typography>
+          </div>
+        )}
+      </Box>
+      <Box mt={4}>
+        <Typography variant="h6">Transaction List</Typography>
+        {transactionData.map((transaction) => (
+          <div key={transaction.id}>
+            <Typography>Payment Mode: {transaction.paymentMode}</Typography>
+            <Typography>Invoice No: {transaction.invoiceNo}</Typography>
+            <Typography>Amount: {transaction.amount}</Typography>
+            <Typography>Currency: {transaction.currency}</Typography>
+            {/* Display other transaction details */}
+            <hr />
+          </div>
+        ))}
+      </Box>
+    </div>
+  );
+}
+
+export default ReportPage;
