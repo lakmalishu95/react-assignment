19f638bd164903e90eea5443e93077753fdc8f64
lakmalishu95 <isurulakmal858@gmail.com>
2023-06-23 11:43:56 +0530

    added login page

---
+++ src/components/LoginPage.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { TextField, Button } from '@mui/material';
+
+const API_ENDPOINT = 'http://3000/api/login';
+
+function LoginPage({ handleLogin }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post(API_ENDPOINT, {
+        username,
+        password,
+      });
+
+      const userData = response.data.user_data;
+      handleLogin(userData);
+    } catch (error) {
+      setError('Invalid username or password.');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <TextField
+            label="Username"
+            variant="outlined"
+            fullWidth
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div>
+          <TextField
+            type="password"
+            label="Password"
+            variant="outlined"
+            fullWidth
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <Button type="submit" variant="contained" fullWidth>
+          Log in
+        </Button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
